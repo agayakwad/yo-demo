f6ac344b085f8b20a9bc53c5f370fb2bf7dba590
anil.ag <anil.ag@hirepo.in>
2013-10-17 12:09:49 +0530

    test(rebase) master 2

--- ui/app/scripts/controllers/index.js
+++ ui/app/scripts/controllers/index.js
@@ -1,7 +1,5 @@
 'use strict';
 angular.module('uiApp').controller('UserIndexCtrl', ['$scope','UserFactory', function ($scope,User) {
-	  
-	
   $scope.users = User.query();
   //$scope.users = User.getUsers()
 }]);
